e195cb3b77468ea832f3eecb356345fa4a47505c
Von <mendresvon@gmail.com>
2026-06-24 15:15:02 +0800

    feat: add prime number checking program to finals_practice

---
+++ finals_practice/problem_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int n;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    bool is_prime = true;
+    if (n <= 1) is_prime = false;
+
+    for (int i=2; i*i <= n; i++){
+        if (n % i == 0){
+            is_prime = false;
+            break;
+        }
+    }
+
+    cout << (is_prime ? "Prime" : "Not a Prime");
+}
