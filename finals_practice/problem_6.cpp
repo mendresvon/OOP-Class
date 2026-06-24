d710483755286ced2f53f7a270b0962d6f9882c8
Von <mendresvon@gmail.com>
2026-06-24 17:06:05 +0800

    refactor: remove bitset header and manually implement binary conversion logic

--- finals_practice/problem_6.cpp
+++ finals_practice/problem_6.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include <bitset>
 
 using namespace std;
 int main(){
     int n;
-    cout << "Enter an integer (-128 ~ 127)";
+    cout << "Enter an integer (-128 ~ 127): ";
     cin >> n;
 
-    cout << bitset<8>(n) << endl;
+    for (int i=7; i>=0; i--){
+        cout << ((n >> i) & 1);
+    }
+    cout << endl;
     return 0;
 }
